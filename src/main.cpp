1db4c9eabb832c33bde84848963bbabd4c9ce985
Arun Felix <arunfelix@Aruns-MacBook-Pro.local>
2024-11-13 19:14:37 -0500

    iot commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+// LED_BUILTIN in connected to pin 25 of the RP2040 chip.
+// It controls the on board LED, at the top-left corner.
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(5000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(5000);
+}
+
